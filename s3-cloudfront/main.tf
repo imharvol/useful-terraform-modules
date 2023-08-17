2aabf1b620f7aa6984f46416d9ce680c1a963bb8
imharvol <imharvol@gmail.com>
2023-08-17 20:15:47 +0200

    Redirect 404 to index.html for SPAs

--- s3-cloudfront/main.tf
+++ s3-cloudfront/main.tf
@@ -127,6 +127,11 @@ resource "aws_cloudfront_distribution" "website" {
     response_code      = 200
     response_page_path = "/index.html"
   }
+  custom_error_response {
+    error_code         = 404
+    response_code      = 404
+    response_page_path = "/index.html"
+  }
 }
 
 # Route 53
